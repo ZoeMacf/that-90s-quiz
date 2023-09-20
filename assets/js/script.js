5e7ad66c6e15dc843b5102f357b9e89c0c426380
ZoeMacf <zoemacfarlane93@gmail.com>
2023-09-20 22:46:31 +0100

    Add an array of objects for storing questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,39 @@
+let currentQuestion = 0;
+let userScore = 0;
+
+let questions = [
+    {
+        question: "Which American rock band, formed in 1990, released the hit song 'Under the Bridge'?",
+        options: ["Foo Fighters", "Weezer", "Red Hot Chili Peppers", "Blink-182"],
+        answer: 2 
+    },
+
+    {
+        question: "What computer game, released in 1991, challenged players to guide a group of green-haired creatures through various obstacles?",
+        options: ["Lemurs", "Monkeys", "Frogger", "Lemmings"],
+        answer: 3
+    },
+
+    {
+        question: "Who played the title character in the 1990s TV show “Buffy the Vampire Slayer”?",
+        options: ["Sarah Michelle Gellar", "Jennifer Love Hewitt", "Alicia Silverstone", "Melissa Joan Hart"],
+        answer: 0
+    },
+
+    {
+        question: "What was the highest-grossing film of the 1990s?",
+        options: ["Shawshank Redemption", "Titanic", "The Bodygyard", "Braveheart"],
+        answer: 1
+    },
+
+    {
+        question: "Which 1990s computer game challenged players to lead a tribe of people through various historical eras by researching technologies, expanding territories, and engaging in diplomacy and warfare?",
+        options: ["Age of Empires", "SimCity", "Myst", "Civilization"],
+        answer: 3
+    }
+]
+
+
 function showRules() {
 
 }
