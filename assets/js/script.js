9e95cf45b5b85326b96cd7850d066c6e40885dab
ZoeMacf <zoemacfarlane93@gmail.com>
2023-09-21 22:27:44 +0100

    Change answer value in questions object to array of answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,55 +1,36 @@
 let questionsArray = [
     {
         question: "Which American rock band, formed in 1990, released the hit song 'Under the Bridge'?",
-        A: "Foo Fighters",
-        B:"Weezer",
-        C: "Red Hot Chili Peppers",
-        D: "Blink-182",
-        answer: 'C'
+        answers: ["Foo Fighters","Weezer","Red Hot Chili Peppers","Blink-182"],
+        correct: 2
     },
 
     {
         question: "What computer game, released in 1991, challenged players to guide a group of green-haired creatures through various obstacles?",
-        A: "Lemurs",
-        B: "Monkeys",
-        C:"Frogger",
-        D: "Lemmings",
-        answer: 'D'
+        answers: ["Lemurs","Monkeys","Frogger","Lemmings"],
+        correct: 3
     },
 
     {
         question: "Who played the title character in the 1990s TV show “Buffy the Vampire Slayer”?",
-        A: "Sarah Michelle Gellar",
-        B: "Jennifer Love Hewitt",
-        C: "Alicia Silverstone",
-        D: "Melissa Joan Hart",
-        answer: 'A'
+        answers: ["Sarah Michelle Gellar","Jennifer Love Hewitt","Alicia Silverstone","Melissa Joan Hart"],
+        answer: 0
     },
 
     {
         question: "What was the highest-grossing film of the 1990s?",
-        A: "Shawshank Redemption",
-        B: "Titanic",
-        C: "The Bodyguard",
-        D: "Braveheart",
-        answer: 'B'
+        answers: ['shawshank', 'titanic', 'bodyguard', 'braveheart'],
+        correct: 1
     },
 
     {
         question: "Which 1990s computer game challenged players to lead a tribe of people through various historical eras by researching technologies, expanding territories, and engaging in diplomacy and warfare?",
-        A: "Age of Empires",
-        B: "SimCity",
-        C: "Myst",
-        D: "Civilization",
-        answer: 'D'
+        answers: ["Age of Empires", "SimCity", "Myst", "Civilization"],
+        correct: 3
     }
 ]
 
 const questionContainer = document.getElementById('question');
-const answerA = document.getElementById('A');
-const answerB= document.getElementById('B');
-const answerC = document.getElementById('C');
-const answerD = document.getElementById('D');
 const nextButton = document.getElementById('next');
 let currentQuestion = 0;
 let userScore = 0;
@@ -72,11 +53,7 @@ function setTimer() {
 
 function displayQuestion() {
     let questionText = questionsArray[currentQuestion];
-    questionContainer.innerHTML = questionText.question;
-    answerA.innerHTML = questionText.A;
-    answerB.innerHTML = questionText.B;
-    answerC.innerHTML = questionText.C;
-    answerD.innerHTML = questionText.D;
+    questionContainer.innerHTML = questionText.question; 
 
 }
 
