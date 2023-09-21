3d81af2e920d43350f1e126b53cc48541f2ef24a
ZoeMacf <zoemacfarlane93@gmail.com>
2023-09-21 11:37:51 +0100

    Change structure of questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,32 +5,47 @@ let choices = document.getElementsByClassName('options');
 let questions = [
     {
         question: "Which American rock band, formed in 1990, released the hit song 'Under the Bridge'?",
-        options: ["Foo Fighters", "Weezer", "Red Hot Chili Peppers", "Blink-182"],
-        answer: 2 
+        A: "Foo Fighters",
+        B:"Weezer",
+        C: "Red Hot Chili Peppers",
+        D: "Blink-182",
+        answer: 'C'
     },
 
     {
         question: "What computer game, released in 1991, challenged players to guide a group of green-haired creatures through various obstacles?",
-        options: ["Lemurs", "Monkeys", "Frogger", "Lemmings"],
-        answer: 3
+        A: "Lemurs",
+        B: "Monkeys",
+        C:"Frogger",
+        D: "Lemmings",
+        answer: 'D'
     },
 
     {
         question: "Who played the title character in the 1990s TV show “Buffy the Vampire Slayer”?",
-        options: ["Sarah Michelle Gellar", "Jennifer Love Hewitt", "Alicia Silverstone", "Melissa Joan Hart"],
-        answer: 0
+        A: "Sarah Michelle Gellar",
+        B: "Jennifer Love Hewitt",
+        C: "Alicia Silverstone",
+        D: "Melissa Joan Hart",
+        answer: 'A'
     },
 
     {
         question: "What was the highest-grossing film of the 1990s?",
-        options: ["Shawshank Redemption", "Titanic", "The Bodygyard", "Braveheart"],
-        answer: 1
+        A: "Shawshank Redemption",
+        B: "Titanic",
+        C: "The Bodyguard",
+        D: "Braveheart",
+        answer: 'B'
     },
 
     {
         question: "Which 1990s computer game challenged players to lead a tribe of people through various historical eras by researching technologies, expanding territories, and engaging in diplomacy and warfare?",
-        options: ["Age of Empires", "SimCity", "Myst", "Civilization"],
-        answer: 3
+        A: "Age of Empires",
+        B: "SimCity",
+        C: "Myst",
+        D: "Civilization",
+        answer: 'D'
     }
 ]
 
