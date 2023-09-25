ba9896083402f0829f9d50f2acbcc862272a0461
ZoeMacf <zoemacfarlane93@gmail.com>
2023-09-25 11:57:26 +0100

    Fix indentation for all files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,113 @@
 let questionsArray = [
-    {
-        question: "Which American rock band, formed in 1990, released the hit song 'Under the Bridge'?",
-        answers: ["Foo Fighters","Weezer","Red Hot Chili Peppers","Blink-182"],
-        correct: "Red Hot Chili Peppers"
-    },
+  {
+    question:
+      "Which American rock band, formed in 1990, released the hit song 'Under the Bridge'?",
+    answers: ["Foo Fighters", "Weezer", "Red Hot Chili Peppers", "Blink-182"],
+    correct: "Red Hot Chili Peppers",
+  },
 
-    {
-        question: "What computer game, released in 1991, challenged players to guide a group of green-haired creatures through various obstacles?",
-        answers: ["Lemurs","Monkeys","Frogger","Lemmings"],
-        correct: "Lemmings"
-    },
+  {
+    question:
+      "What computer game, released in 1991, challenged players to guide a group of green-haired creatures through various obstacles?",
+    answers: ["Lemurs", "Monkeys", "Frogger", "Lemmings"],
+    correct: "Lemmings",
+  },
 
-    {
-        question: "Who played the title character in the 1990s TV show “Buffy the Vampire Slayer”?",
-        answers: ["Sarah Michelle Gellar", "Jennifer Love Hewitt","Alicia Silverstone","Melissa Joan Hart"],
-        correct: "Sarah Michelle Gellar"
-    },
+  {
+    question:
+      "Who played the title character in the 1990s TV show “Buffy the Vampire Slayer”?",
+    answers: [
+      "Sarah Michelle Gellar",
+      "Jennifer Love Hewitt",
+      "Alicia Silverstone",
+      "Melissa Joan Hart",
+    ],
+    correct: "Sarah Michelle Gellar",
+  },
 
-    {
-        question: "What was the highest-grossing film of the 1990s?",
-        answers: ['Shawshank', 'Titanic', 'Bodyguard', 'Braveheart'],
-        correct: 'Titanic'
-    },
+  {
+    question: "What was the highest-grossing film of the 1990s?",
+    answers: ["Shawshank", "Titanic", "Bodyguard", "Braveheart"],
+    correct: "Titanic",
+  },
 
-    {
-        question: "Which 1990s computer game challenged players to lead a tribe of people through various historical eras by researching technologies, expanding territories, and engaging in diplomacy and warfare?",
-        answers: ["Age of Empires", "SimCity", "Myst", "Civilization"],
-        correct: "Civilization"
-    }
-]
+  {
+    question:
+      "Which 1990s computer game challenged players to lead a tribe of people through various historical eras by researching technologies, expanding territories, and engaging in diplomacy and warfare?",
+    answers: ["Age of Empires", "SimCity", "Myst", "Civilization"],
+    correct: "Civilization",
+  },
+];
 
-const startQuizContainer = document.getElementById('quiz-start');
-const gameContainer = document.getElementById('game-content');
-const questionContainer = document.getElementById('question-container');
-const questionContent = document.getElementById('question-content');
-const nextButton = document.getElementById('next');
-const startButton = document.getElementById('start-btn');
-const answerContainer = document.getElementById('answer-container');
-const finalScore = document.getElementById('final-score');
+const startQuizContainer = document.getElementById("quiz-start");
+const gameContainer = document.getElementById("game-content");
+const questionContainer = document.getElementById("question-container");
+const questionContent = document.getElementById("question-content");
+const nextButton = document.getElementById("next");
+const startButton = document.getElementById("start-btn");
+const answerContainer = document.getElementById("answer-container");
+const finalScore = document.getElementById("final-score");
 
 let currentQuestion = 0;
 let userScore = 0;
 
-document.addEventListener('DOMContentLoaded', function() {
-    questionContainer.classList.add('hide');
-    startButton.addEventListener('click', startQuiz);
-
-})
+document.addEventListener("DOMContentLoaded", function () {
+  questionContainer.classList.add("hide");
+  startButton.addEventListener("click", startQuiz);
+});
 
 /**
- * Sets currentQuestion and userScore to 0 and will then display the question. 
+ * Sets currentQuestion and userScore to 0 and will then display the question.
  */
 function startQuiz() {
-    questionContainer.classList.remove('hide');
-    startQuizContainer.classList.add('hide');
+  questionContainer.classList.remove("hide");
+  startQuizContainer.classList.add("hide");
 
-    currentQuestion = 0;
-    userScore = 0;
-    displayQuestion();
+  currentQuestion = 0;
+  userScore = 0;
+  displayQuestion();
 }
 
 /**
  * Sets the value questionText to be equal to questionsArray with the index set to currentQuestion value.
- * The questionContainer.innerHTML is set to equal the question at that index value. 
+ * The questionContainer.innerHTML is set to equal the question at that index value.
  * To get the answers the function will then iterate through the array of answers, create a button for each one
  * and append to the answer-container div.
  */
 function displayQuestion() {
-    let questionText = questionsArray[currentQuestion];
-    questionContent.innerHTML = questionText.question;
-    let answers = questionsArray[currentQuestion].answers;
-    answers.forEach(answer => {
-        let button = document.createElement('button');
-        button.innerHTML = answer;
-        button.classList.add('answer-btn');
-        answerContainer.appendChild(button);
-    })
-    }
+  let questionText = questionsArray[currentQuestion];
+  questionContent.innerHTML = questionText.question;
+  let answers = questionsArray[currentQuestion].answers;
+  answers.forEach((answer) => {
+    let button = document.createElement("button");
+    button.innerHTML = answer;
+    button.classList.add("answer-btn");
+    answerContainer.appendChild(button);
+  });
+}
 /**This function will check to see if the answerContainer has a firstChild appended
- * if this is the case it will remove it and then run the displayQuestion function. 
+ * if this is the case it will remove it and then run the displayQuestion function.
  */
 function resetState() {
-    while(answerContainer.firstChild){
-        answerContainer.removeChild(answerContainer.firstChild);
-    }
-    displayQuestion();
+  while (answerContainer.firstChild) {
+    answerContainer.removeChild(answerContainer.firstChild);
+  }
+  displayQuestion();
 }
 
 /**This function will increment currentQuestion by 1 then check to see if it is less than the length
  * of questionArray, if this is the case it will display the next question then call resetState.
  */
 function nextQuestion() {
-    currentQuestion++;
-    if(currentQuestion < questionsArray.length){
-        displayQuestion();
-        resetState();
-    } else {
-        nextButton.classList.add('hide');
-        showFinalScore();
-    }
-}  
+  currentQuestion++;
+  if (currentQuestion < questionsArray.length) {
+    displayQuestion();
+    resetState();
+  } else {
+    nextButton.classList.add("hide");
+    showFinalScore();
+  }
+}
 
 /**
  * This function will create a button element with the content 'Finish Quiz', it will then wait for the user to click
@@ -107,49 +115,48 @@ function nextQuestion() {
  * the user's final score and display a button to restart quiz.
  */
 function showFinalScore() {
-        let finishButton = document.createElement('button');
-        finishButton.innerText = 'Finish Quiz';
-        finishButton.classList.add('finish-btn');
-        questionContainer.append(finishButton);
+  let finishButton = document.createElement("button");
+  finishButton.innerText = "Finish Quiz";
+  finishButton.classList.add("finish-btn");
+  questionContainer.append(finishButton);
 
-        finishButton.addEventListener('click', function() {
-         questionContainer.classList.add('hide');
-         let scoreHeading = document.createElement('h2');
+  finishButton.addEventListener("click", function () {
+    questionContainer.classList.add("hide");
+    let scoreHeading = document.createElement("h2");
 
-         scoreHeading.innerText = `You answered ${userScore} out of ${currentQuestion} correct!`;
-         finalScore.classList.remove('hide');
+    scoreHeading.innerText = `You answered ${userScore} out of ${currentQuestion} correct!`;
+    finalScore.classList.remove("hide");
 
-         finalScore.classList.add('show-final-score');
-         finalScore.append(scoreHeading);
+    finalScore.classList.add("show-final-score");
+    finalScore.append(scoreHeading);
 
-         let restartQuiz = document.createElement('button');
-         restartQuiz.classList.add('finish-btn');
-         restartQuiz.innerText = 'Restart Quiz';
-         finalScore.append(restartQuiz);
-        });
+    let restartQuiz = document.createElement("button");
+    restartQuiz.classList.add("finish-btn");
+    restartQuiz.innerText = "Restart Quiz";
+    finalScore.append(restartQuiz);
+  });
 }
 //Code used to check user button input was found here https://stackoverflow.com/questions/66193592/how-i-know-which-button-been-clicked-in-class-of-buttons
 /**
  * Function contains an event listener to check which of the answer buttons are clicked, if it is correct it will add to the score
- * and highlight the answer as being correct, if incorrect score is not affected and answer will be highlighted to show incorrect. 
+ * and highlight the answer as being correct, if incorrect score is not affected and answer will be highlighted to show incorrect.
  */
 function checkAnswer() {
-    answerContainer.addEventListener('click', function(evt) {
-        let userInput = evt.target;
-        let correctAnswers = questionsArray[currentQuestion].correct;
-        if(userInput.textContent === correctAnswers){
-            addToScore();
-            userInput.classList.add('correct-answer');
-        } else {
-            userInput.classList.add('wrong-answer');
-        }
-      });
-
+  answerContainer.addEventListener("click", function (evt) {
+    let userInput = evt.target;
+    let correctAnswers = questionsArray[currentQuestion].correct;
+    if (userInput.textContent === correctAnswers) {
+      addToScore();
+      userInput.classList.add("correct-answer");
+    } else {
+      userInput.classList.add("wrong-answer");
+    }
+  });
 }
 
 function addToScore() {
-    userScore++;
+  userScore++;
 }
 
-nextButton.addEventListener('click', nextQuestion);
+nextButton.addEventListener("click", nextQuestion);
 checkAnswer();
